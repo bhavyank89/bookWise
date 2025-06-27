afc5cf18c2235739d2077f0321e1ea60d92fe9a3
bhavyank89 <bhavyanksarolia@gmail.com>
2025-06-27 13:35:47 +0530

    add vercel stats in frontend

--- frontend-dev/src/main.jsx
+++ frontend-dev/src/main.jsx
@@ -1,7 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <Analytics>
+        <App />
+    </Analytics>
 )
